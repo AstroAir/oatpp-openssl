ea6d3a6a8bd7d86c9351a570d311ecd678eb6646
Max Qian <64824374+AstroAir@users.noreply.github.com>
2024-11-02 00:14:03 +0800

    Update TemporaryDhParamsFile.cpp

--- src/oatpp-openssl/configurer/TemporaryDhParamsFile.cpp
+++ src/oatpp-openssl/configurer/TemporaryDhParamsFile.cpp
@@ -37,24 +37,33 @@ void TemporaryDhParamsFile::configure(SSL_CTX* ctx) {
                                  "Call to 'BIO_new_file' failed.");
     }
 
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     std::shared_ptr<EVP_PKEY> pkey(PEM_read_bio_PrivateKey(bio.get(), nullptr, nullptr, nullptr), EVP_PKEY_free);
     if (!pkey) {
         throw std::runtime_error("[oatpp::openssl::configurer::TemporaryDhParamsFile::configure()]: Error. "
                                  "Call to 'PEM_read_bio_PrivateKey' failed.");
     }
-
     std::shared_ptr<EVP_PKEY> dh(EVP_PKEY_new(), EVP_PKEY_free);
+#else
+    std::shared_ptr<DH> dh(PEM_read_bio_DHparams(bio.get(), 0, 0, 0), DH_free);
+#endif
     if (!dh) {
         throw std::runtime_error("[oatpp::openssl::configurer::TemporaryDhParamsFile::configure()]: Error. "
                                  "Call to 'EVP_PKEY_new' failed.");
     }
 
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     if (EVP_PKEY_copy_parameters(dh.get(), pkey.get()) <= 0) {
         throw std::runtime_error("[oatpp::openssl::configurer::TemporaryDhParamsFile::configure()]: Error. "
                                  "Call to 'EVP_PKEY_copy_parameters' failed.");
     }
+#endif
 
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     if (SSL_CTX_set0_tmp_dh_pkey(ctx, dh.get()) <= 0) {
+#else
+    if (SSL_CTX_set_tmp_dh(ctx, dh.get()) <= 0) {
+#endif
         throw std::runtime_error("[oatpp::openssl::configurer::TemporaryDhParamsFile::configure()]: Error. "
                                  "Call to 'SSL_CTX_set0_tmp_dh_pkey' failed.");
     }
